102036c5d6bfc50d153b0752c4db0ca086173ab1
Jozef Izso <jozef.izso@gmail.com>
2014-08-15 13:42:34 +0200

    Added project for the DDColorConsoleLogger library

---
+++ DDColorConsoleLogger/DDColorConsoleLogger.h
@@ -0,0 +1,12 @@
+//
+//  DDColorConsoleLogger.h
+//  DDColorConsoleLogger
+//
+//  Copyright (c) 2014 Jozef Izso. MIT license.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDColorConsoleLogger : NSObject
+
+@end
---
+++ DDColorConsoleLogger/DDColorConsoleLogger.m
@@ -0,0 +1,12 @@
+//
+//  DDColorConsoleLogger.m
+//  DDColorConsoleLogger
+//
+//  Copyright (c) 2014 Jozef Izso. MIT license.
+//
+
+#import "DDColorConsoleLogger.h"
+
+@implementation DDColorConsoleLogger
+
+@end
