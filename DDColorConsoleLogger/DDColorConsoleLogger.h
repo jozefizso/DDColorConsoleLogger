781e4d2d61689d4c377a67514dcfb1daeeca0377
Jozef Izso <jozef.izso@gmail.com>
2014-08-15 17:19:46 +0200

    Added documentation.

--- DDColorConsoleLogger/DDColorConsoleLogger.h
+++ DDColorConsoleLogger/DDColorConsoleLogger.h
@@ -10,6 +10,12 @@
 
 @interface DDColorConsoleLogger : NSObject
 
+/**
+ *  Configures the DDTTYLogger shared instance to use colorized output
+ *  and sets colors to Tomorrow theme.
+ *
+ *  @return Returns DDTTYLogger shared instance configured to use colorized output.
+ */
 + (DDTTYLogger *)sharedInstance;
 
 @end
